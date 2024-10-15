8af16a19dfb5398ff2acc84620030bd436778e31
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-10-15 12:21:25 +0530

    wip

---
+++ tests/Unit/UnfoldTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hyvor\Unfold\Tests\Unit;
+
+use Hyvor\Unfold\Types\EmbedTypeEnum;
+use Hyvor\Unfold\Unfold;
+
+it('handles oEmbed supported URLs', function () {
+
+    $url = 'https://www.youtube.com/watch?v=c0uLtuZufrg';
+    $data = Unfold::unfold($url);
+
+    expect($data->type)->toBe(EmbedTypeEnum::VIDEO);
+    expect($data->author_url)->toBe('https://www.youtube.com/@Team-2FORTY2');
+    expect($data)->toBe('hi');
+
+});
