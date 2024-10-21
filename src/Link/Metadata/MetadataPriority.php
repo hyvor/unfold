ee66127d27339d3b8591c9247555e8e57e2efb0a
SupunKavinda <supunkavinda1125@gmail.com>
2024-10-21 23:07:53 +0530

    metadata priority

---
+++ src/Link/Metadata/MetadataPriority.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Hyvor\Unfold\Link\Metadata;
+
+use Hyvor\Unfold\Unfolded\UnfoldedAuthor;
+use DateTimeInterface;
+use Hyvor\Unfold\Unfolded\UnfoldedTag;
+
+/**
+ * This class select the best metadata to use in the Unfolded object
+ */
+class MetadataPriority
+{
+
+    /**
+     * @param MetadataObject[] $metadata
+     */
+    public function __construct(private array $metadata)
+    {
+    }
+
+    /**
+     * @param MetadataKeyType[] $keys
+     */
+    private function prioritizedAll(array $keys)
+    {
+        $keyedMetadata = [];
+
+        /**
+         * First, we group the metadata by their key and filters metadata we don't want
+         * Each key may have multiple metadata
+         *
+         * [
+         *     'OG_TITLE' => [MetadataObject],
+         *     'TITLE' => [MetadataObject, MetadataObject],
+         * ]
+         */
+        foreach ($this->metadata as $metadata) {
+            $key = $metadata->key;
+            if (!in_array($key, $keys)) {
+                continue;
+            }
+
+            $keyName = $key->name;
+            if (isset($keyedMetadata[$keyName])) {
+                $keyedMetadata[$keyName][] = $metadata;
+            } else {
+                $keyedMetadata[$keyName] = [$metadata];
+            }
+        }
+
+        /**
+         * Next, we are sorting $keyedMetadata by the order of $keys
+         * If $keys was [TITLE, OG_TITLE], then the order of $keyedMetadata will be
+         * [
+         *    'TITLE' => [MetadataObject, MetadataObject],
+         *    'OG_TITLE' => [MetadataObject],
+         * ]
+         */
+        $keysNames = array_map(fn($key) => $key->name, $keys);
+        uksort($keyedMetadata, function ($a, $b) use ($keysNames) {
+            return array_search($a, $keysNames) - array_search($b, $keysNames);
+        });
+
+        // index by 0,1,2
+        $keyedMetadata = array_values($keyedMetadata);
+        // return the values
+        return array_map(fn($metadata) => $metadata->value, $keyedMetadata[0] ?? []);
+    }
+
+    /**
+     * @param MetadataKeyType[] $keys
+     * @return mixed|null
+     */
+    private function prioritized(array $keys)
+    {
+        return $this->prioritizedAll($keys)[0] ?? null;
+    }
+
+    public function title(): ?string
+    {
+        return $this->prioritized(
+            [
+                MetadataKeyType::TITLE,
+                MetadataKeyType::OG_TITLE,
+                MetadataKeyType::TWITTER_TITLE
+            ]
+        );
+    }
+
+    public function description(): ?string
+    {
+        return $this->prioritized([
+            MetadataKeyType::DESCRIPTION,
+            MetadataKeyType::OG_DESCRIPTION,
+            MetadataKeyType::TWITTER_DESCRIPTION
+        ]);
+    }
+
+    /**
+     * @return UnfoldedAuthor[]
+     */
+    public function authors()
+    {
+        return $this->prioritizedAll([
+            MetadataKeyType::RICH_SCHEMA_AUTHOR,
+            MetadataKeyType::OG_ARTICLE_AUTHOR,
+            MetadataKeyType::TWITTER_CREATOR
+        ]);
+    }
+
+    /**
+     * @return UnfoldedTag[]
+     */
+    public function tags(): array
+    {
+        return $this->prioritizedAll([
+            MetadataKeyType::OG_ARTICLE_TAG
+        ]);
+    }
+
+
+    public function siteName(): ?string
+    {
+        return $this->prioritized([
+            MetadataKeyType::OG_SITE_NAME,
+            MetadataKeyType::TWITTER_SITE
+        ]);
+    }
+
+
+    public function siteUrl(): ?string
+    {
+        return $this->prioritized([
+            MetadataKeyType::OG_URL
+        ]);
+    }
+
+
+    public function canonicalUrl(): ?string
+    {
+        return $this->prioritized([
+            MetadataKeyType::CANONICAL_URL
+        ]);
+    }
+
+
+    public function publishedTime(): ?DateTimeInterface
+    {
+        return $this->prioritized([
+            MetadataKeyType::RICH_SCHEMA_PUBLISHED_TIME,
+            MetadataKeyType::OG_ARTICLE_PUBLISHED_TIME
+        ]);
+    }
+
+
+    public function modifiedTime(): ?DateTimeInterface
+    {
+        return $this->prioritized([
+            MetadataKeyType::RICH_SCHEMA_MODIFIED_TIME,
+            MetadataKeyType::OG_ARTICLE_MODIFIED_TIME
+        ]);
+    }
+
+
+    public function thumbnailUrl(): ?string
+    {
+        return $this->prioritized([
+            MetadataKeyType::OG_IMAGE,
+            MetadataKeyType::OG_IMAGE_URL,
+            MetadataKeyType::OG_IMAGE_SECURE_URL,
+            MetadataKeyType::TWITTER_IMAGE
+        ]);
+    }
+
+
+    public function iconUrl(): ?string
+    {
+        return $this->prioritized([
+            MetadataKeyType::FAVICON_URL
+        ]);
+    }
+
+
+    public function locale(): ?string
+    {
+        return $this->prioritized([
+            MetadataKeyType::LOCALE,
+            MetadataKeyType::OG_LOCALE
+        ]);
+    }
+
+}
--- tests/Feature/UnfoldTest.php
+++ tests/Feature/UnfoldTest.php
@@ -1,11 +1,17 @@
 <?php
 
-it('gets link', function () {
-    //
+use Hyvor\Unfold\Unfold;
+use Hyvor\Unfold\UnfoldMethod;
 
+it('gets link', function () {
+    $response = Unfold::unfold('https://talk.hyvor.com');
+    dd($response);
 });
 
 it('gets embed', function () {
-    //
-
+    $response = Unfold::unfold(
+        'https://www.youtube.com/watch?v=8scL5oJX6CM',
+        UnfoldMethod::EMBED,
+    );
+    dd($response);
 });
