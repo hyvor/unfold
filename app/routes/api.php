69dd1bd7722bea2e33adbbbdf03ac027090b52de
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-10-24 12:01:01 +0530

    api endpoint

--- app/routes/api.php
+++ app/routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/unfold', [Controller::class, 'init']);
+Route::get('iframe', [Controller::class, 'iframe']);
